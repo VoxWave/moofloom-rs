2d8d9b405eb5e4165d801944493fb680fb1c6a96
Victor Bankowski <victor_bankowski@hotmail.com>
2017-11-15 00:15:11 +0200

    added commands. Changed MemoryType to Param to better reflect the usage of the enum. Added registers and program counter to the machine

--- src/vm.rs
+++ src/vm.rs
@@ -1,11 +1,17 @@
 pub struct Machine {
-    code: Vec<Command>
+    code: Vec<Command>,
+    registers: Vec<u64>,
+    program_counter: u64,
 }
 
 enum Command {
-    Add(MemoryType, MemoryType, MemoryType), Mul, Div,
+    fAdd(Param, Param, Param),
+    fSub(Param, Param, Param), 
+    fMul(Param, Param, Param), 
+    fDiv(Param, Param, Param),
+    Load(Param, Param),
 }
 
-enum MemoryType {
-    Register(usize),
+enum Param {
+    Register(usize), fConstant(f64),
 }
