d9be82908449654f0b96eecb986352dc92824b20
Victor Bankowski <victor_bankowski@hotmail.com>
2017-12-08 22:45:11 +0200

    moo parsing error handling changed from deliberate crashing to Results

--- src/moo/mod.rs
+++ src/moo/mod.rs
@@ -3,19 +3,26 @@ use std::io::Read;
 use Program;
 use vm::Command;
 
-pub fn parse_program<R: Read>(r: R) -> Program {
+pub fn parse_program<R: Read>(r: R) -> Result<Program, MooParseError> {
     let mut source = String::new();
     r.read_to_string(&mut source).expect("IO error! Reading source file into string failed");
+    let mut program = Vec::new();
 
-    for s in source.split(';') {
-        let c: Vec<_> = s.split(' ').collect();
-        match &*c[0].to_lowercase() {
-            "fadd" => {},
+    for instruction in source.split(';') {
+        let params: Vec<_> = instruction.split(' ').collect();
+        match &*params[0].to_lowercase() {
+            "fadd" => {
+                
+            },
             "fsub" => {},
             "fmul" => {},
             "fdiv" => {},
             "load" => {},
-            _ => unimplemented!("Command does not exist."),
+            _ => return Err(MooParseError::CommandNotFound(instruction.to_string())),
         }
     }
+}
+
+enum MooParseError {
+    CommandNotFound(String), IOError(::std::io::Error), InsufficientParamAmount
 }
