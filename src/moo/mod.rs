4054c59761c40181b45dd4ef3dc14b019040fafe
Victor Bankowski <victor_bankowski@hotmail.com>
2017-12-10 23:13:27 +0200

    parsing code advanced

--- src/moo/mod.rs
+++ src/moo/mod.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 
 use Program;
-use vm::Command;
+use vm::{Command, Param};
 
 pub fn parse_program<R: Read>(r: R) -> Result<Program, MooParseError> {
     let mut source = String::new();
@@ -10,19 +10,35 @@ pub fn parse_program<R: Read>(r: R) -> Result<Program, MooParseError> {
 
     for instruction in source.split(';') {
         let params: Vec<_> = instruction.split(' ').collect();
-        match &*params[0].to_lowercase() {
-            "fadd" => {
-                
+        match &*params[0].to_lowercase().trim() {
+            i @ "fadd" | "fsub" | "fmul" | "fdiv" => {
+                if let (p1, p2, p3 @ Param::Register(_)) = parse_three_params(params)? {
+                    match i {
+                        "fadd" => program.push(Command::FAdd(p1, p2, p3)),
+                        "fsub" => program.push(Command::FSub(p1, p2, p3)),
+                        "fmul" => program.push(Command::FMul(p1, p2, p3)),
+                        "fdiv" => program.push(Command::FDiv(p1, p2, p3)),
+                    }
+                } else {
+                    return Err(MooParseError::InvalidSyntax(format!("Third parameter in \"{}\" should be a register", instruction.to_string())));
+                }
             },
-            "fsub" => {},
-            "fmul" => {},
-            "fdiv" => {},
-            "load" => {},
+            "load" => {
+                return Err(MooParseError::CommandNotFound);
+            },
             _ => return Err(MooParseError::CommandNotFound(instruction.to_string())),
         }
     }
 }
 
+pub fn parse_three_params(params: &Vec<str>) -> Result<(Param, Param, Param), MooParseError> {
+    Err(MooParseError::CommandNotFound)
+}
+
 enum MooParseError {
-    CommandNotFound(String), IOError(::std::io::Error), InsufficientParamAmount
+    CommandNotFound(String), 
+    IOError(::std::io::Error), 
+    InsufficientParamAmount,
+    InvalidParam(String),
+    InvalidSyntax(String),
 }
