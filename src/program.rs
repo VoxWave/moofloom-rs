6245bc391a16d7d2975176c72161e705a91ca27b
Your Name <victor_bankowski@hotmail.com>
2018-11-23 22:24:09 +0200

    program struct added

--- src/program.rs
+++ src/program.rs
@@ -3,7 +3,15 @@ use std::ops::Index;
 
 use vm::Command;
 
-struct Program {
+pub struct Program {
     program: Vec<Command>,
     labels: HashMap<String, usize>,
+}
+
+impl Index<usize> for Program {
+    type Output = Command;
+    
+    fn index(&self, i: usize) -> &Command {
+        &self.program[i]
+    }
 }
