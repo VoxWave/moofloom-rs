36f2a5298216e4c4f61c1c8b4260775f094dfe8f
Your Name <victor_bankowski@hotmail.com>
2019-01-12 01:00:24 +0200

    added getters for MooMachine in the test module

--- src/vm/vm_test.rs
+++ src/vm/vm_test.rs
@@ -1,1 +1,26 @@
+use common::{Sink, Source};
+use program::Program;
+use std::collections::HashMap;
 
+use super::MooMachine;
+
+impl MooMachine {
+    fn get_program(&self) -> &Program {
+        &self.program
+    }
+
+    fn get_registers(&self) -> &HashMap<u64, u64> {
+        &self.registers
+    }
+
+    fn get_program_counter(&self) -> u64 {
+        self.program_counter
+    }
+
+    fn get_inputs(&self) -> &Vec<Box<Source<u64>>> {
+        &self.input
+    }
+    fn get_outputs(&self) -> &Vec<Box<Sink<u64>>> {
+        &self.output
+    } 
+}
