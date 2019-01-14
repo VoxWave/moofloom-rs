6d6f1e3642a3e036a0a519874e006bc27a3b6328
VoxWave <victor_bankowski@hotmail.com>
2019-01-14 16:00:08 +0200

    started on a vm test

--- src/vm/vm_test.rs
+++ src/vm/vm_test.rs
@@ -2,6 +2,7 @@ use common::{Sink, Source};
 use program::Program;
 use std::collections::HashMap;
 
+use crate::moo::parse_program_from_string
 use super::MooMachine;
 
 impl MooMachine {
@@ -23,4 +24,14 @@ impl MooMachine {
     fn get_outputs(&self) -> &Vec<Box<Sink<u64>>> {
         &self.output
     } 
+}
+
+#[test]
+fn load_float_test() {
+    let program = parse_program_from_string("load 3f R1;");
+    let machine = MooMachine::new(program: Program, Vec::new(), Vec::new());
+    assert_eq!(machine.get_registers().get(1), 0.);
+    machine.tick();
+    assert_eq!(machine.get_registers().get(1), 1.)
+
 }
